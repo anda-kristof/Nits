df1b357de95cb842f60e0baddaa72a0ca78ef7bc
Luo-Hong-Jun <luo.hong.jun@students.jedlik.eu>
2023-12-29 22:04:03 +0100

    Javacript basics almost finished

--- FuggvenyBajnok.js
+++ FuggvenyBajnok.js
@@ -1,19 +1,101 @@
-const Box1 = document.querySelector("box1");
-const Box2 = document.querySelector("box2");
-const Box3 = document.querySelector("box3");
-const Box4 = document.querySelector("box4");
+import {Questions} from "./QuestionList.js";
+
+const nitsReactions = ["Nagyon Lassan válaszolsz Vagy gyerekem , nagyanyám is gyorsabb volt nálad! FAILURE!!!!", "Hát fiacskám ez nagyon lassú volt hogy leszel így Függvény Bajnok? Most azonnal sétálj az asztal körül 1 kört!!!", "Ha ilyen sebességgel haladsz tovább akkor egy alát fogsz kapni!","Azt hiszed hogy Chuck Norris vagy aki 0 után is tud quizt csinálni?"];
+
+let Box1 = document.querySelector("#box1");
+let Box2 = document.querySelector("#box2");
+let Box3 = document.querySelector("#box3");
+let Box4 = document.querySelector("#box4");
+let Said = false;
+let audioActive = true; // csakhogy ne halljam a nits jumpscaret egyfolytában
+let TappedNum = 0;
+
 
 function Counter() {
     let num = document.querySelector("#counter").innerHTML;
     num--;
-    if (num < 6) {
-    document.querySelector("#reaction").innerHTML = "Nagyon Lassan válaszolsz Vagy gyerekem , nagyanyám is gyorsabb volt nálad! FAILURE!!!!";
+    if (num < 0) {
+        alert("VESZTETTÉL BITCH")
+    }
+    if (num < 6 && Said == false) {
+    Said = true;
+    let randomNum = Math.floor(Math.random() * 5);
+    document.querySelector("#reaction").innerHTML = nitsReactions[randomNum];
+    }
+    else if (num < 0 && audioActive == false){
+        document.querySelector("#audio").innerHTML += '<audio src="/Whistle.mp3" controls="controls" style="display: none;" autoplay></audio>';
+        audioActive = true;
     }
     document.querySelector("#counter").innerHTML = num;
 };
 
 setInterval(Counter,1000);
 
-for (let index = 0; index < 11; index++) {
-    addEventListener()
-}
+
+
+    Box1.addEventListener('click', function() {
+        let TappedButtonValue = document.querySelector('#answer1').innerHTML;
+        if (TappedButtonValue == Questions[TappedNum].answer) {
+            TappedNum++;
+            document.querySelector('#box1').innerHTML = Questions[TappedNum].question1;
+            document.querySelector('#box2').innerHTML = Questions[TappedNum].question2;
+            document.querySelector('#box3').innerHTML = Questions[TappedNum].question3;
+            document.querySelector('#box4').innerHTML = Questions[TappedNum].question4;
+            document.querySelector('#Difficulty').innerHTML = Questions[TappedNum].difficulty;
+            Said = false;
+            document.querySelector('#counter').innerHTML = 12;
+        }
+        console.log(TappedButtonValue);
+        console.log(TappedNum);
+      });
+
+
+
+    Box2.addEventListener('click', function() {
+        let TappedButtonValue = document.querySelector('#answer2').innerHTML;
+        if (TappedButtonValue == Questions[TappedNum].answer) {
+            TappedNum++;
+            document.querySelector('#box1').innerHTML = Questions[TappedNum].question1;
+            document.querySelector('#box2').innerHTML = Questions[TappedNum].question2;
+            document.querySelector('#box3').innerHTML = Questions[TappedNum].question3;
+            document.querySelector('#box4').innerHTML = Questions[TappedNum].question4;
+            document.querySelector('#Difficulty').innerHTML = Questions[TappedNum].difficulty;
+            document.querySelector('#counter').innerHTML = 12;
+            Said = false;
+        }
+        console.log('Div2 was clicked!');
+      });
+
+
+
+    Box3.addEventListener('click', function() {
+        let TappedButtonValue = document.querySelector('#answer3').innerHTML;
+        if (TappedButtonValue == Questions[TappedNum].answer) {
+            TappedNum++;
+            document.querySelector('#box1').innerHTML = Questions[TappedNum].question1;
+            document.querySelector('#box2').innerHTML = Questions[TappedNum].question2;
+            document.querySelector('#box3').innerHTML = Questions[TappedNum].question3;
+            document.querySelector('#box4').innerHTML = Questions[TappedNum].question4;
+            document.querySelector('#Difficulty').innerHTML = Questions[TappedNum].difficulty;
+            document.querySelector('#counter').innerHTML = 12;
+            Said = false;
+        }
+        console.log('Div3 was clicked!');
+      });
+
+
+
+    Box4.addEventListener('click', function() {
+        let TappedButtonValue = document.querySelector('#answer4').innerHTML;
+        if (TappedButtonValue == Questions[TappedNum].answer) {
+            TappedNum++;
+            document.querySelector('#box1').innerHTML = Questions[TappedNum].question1;
+            document.querySelector('#box2').innerHTML = Questions[TappedNum].question2;
+            document.querySelector('#box3').innerHTML = Questions[TappedNum].question3;
+            document.querySelector('#box4').innerHTML = Questions[TappedNum].question4;
+            document.querySelector('#Difficulty').innerHTML = Questions[TappedNum].difficulty;
+            document.querySelector('#counter').innerHTML = 12;
+            Said = false;
+        }
+        console.log('Div4 was clicked!');
+      });
---
+++ QuestionList.js
@@ -0,0 +1,87 @@
+const Questions =
+[
+    {
+      'difficulty': 'Nagyon könnyű',
+      'question1': 'george.bluth@reqres.in',
+      'question2': 'George',
+      'question3': 'Bluth',
+      'question4': 'https://reqres.in/img/faces/1-image.jpg',
+      'answer': 'Nits László'
+    },
+    {
+        'difficulty': 'Könnyű',
+      'question1': '<p id="answer1">janet.weaver@reqres.in</p>',
+      'question2': '<p id="answer2">Janet</p>',
+      'question3': '<p id="answer3">Chuck Laci</p>',
+      'question4': '<p id="answer4">Nitsubishi</p>',
+      'answer': 'janet.weaver@reqres.in'
+    },
+    {
+        'difficulty': 'Könnyű',
+      'question1': '<p id="answer1">emma.wong@reqres.in</p>',
+      'question2': '<p id="answer1">Emma</p>',
+      'question3': '<p id="answer1">Wong</p>',
+      'question4': '<p id="answer1">https://reqres.in/img/faces/3-image.jpg</p>',
+      'answer': 'bruh'
+    },
+    {
+        'difficulty': 'Közepes',
+      'question1': 'eve.holt@reqres.in',
+      'question2': 'Eve',
+      'question3': 'Holt',
+      'question4': 'https://reqres.in/img/faces/4-image.jpg',
+      'answer': 'bruh'
+    },
+    {
+        'difficulty': 'Közepes',
+      'question1': 'charles.morris@reqres.in',
+      'question2': 'Charles',
+      'question3': 'Morris',
+      'question4': 'https://reqres.in/img/faces/5-image.jpg',
+      'answer': 'bruh'
+    },
+    {
+        'difficulty': 'Nehéz',
+      'question1': 'byron.fields@reqres.in',
+      'question2': 'Byron',
+      'question3': 'Fields',
+      'question4': 'https://reqres.in/img/faces/10-image.jpg',
+      'answer': 'bruh'
+    },
+    {
+        'difficulty': 'Nehéz',
+        'question1': 'george.bluth@reqres.in',
+        'question2': 'George',
+        'question3': 'Bluth',
+        'question4': 'https://reqres.in/img/faces/1-image.jpg',
+        'answer': 'bruh'
+      },
+      {
+        'difficulty': 'Nagyon nehéz',
+        'question1': 'janet.weaver@reqres.in',
+        'question2': 'Janet',
+        'question3': 'Weaver',
+        'question4': 'https://reqres.in/img/faces/2-image.jpg',
+        'answer': 'bruh'
+      },
+      {
+        'difficulty': 'Nagyon nehéz',
+        'question1': 'emma.wong@reqres.in',
+        'question2': 'Emma',
+        'question3': 'Wong',
+        'question4': 'https://reqres.in/img/faces/3-image.jpg',
+        'answer': 'bruh'
+      },
+      {
+        'difficulty': 'Szuper könnyű',
+        'question1': 'eve.holt@reqres.in',
+        'question2': 'Eve',
+        'question3': 'Holt',
+        'question4': 'https://reqres.in/img/faces/4-image.jpg',
+        'answer': 'bruh'
+      }
+]
+
+export  {
+  Questions
+}
